3653c2fc9b731626586b797e66e3406644eff434
kwwalton <kennethwwalton@gmail.com>
2022-02-05 11:16:32 -0500

    chore: got vuex actions woring with jest
    - had to define repository plugin object in the test

--- test/store/users.spec.js
+++ test/store/users.spec.js
@@ -1,6 +1,7 @@
-import { mutations } from '~/store/users'
+import { mutations, actions } from '~/store/users'
+import $repository from '~/plugins/api'
 
-// TODO: test actions, have a setup function to reset state and have one state for all tests
+// TODO: use beforeEach to set state
 
 describe('store/users', () => {
   test('mutations: isLoadingUsers', () => {
@@ -23,5 +24,46 @@ describe('store/users', () => {
     ]
     mutations.setUsers(state, mockResponse)
     expect(state.users).toEqual(mockResponse)
+  })
+
+  test('actions: getUsers - Happy Path', async () => {
+    const state = {
+      users: [],
+      isLoadingUsers: false,
+    }
+    const mockResponse = [
+      { id: 1, name: 'Pierre' },
+      { id: 2, name: 'Jared' },
+    ]
+    const commit = jest.fn()
+
+    actions.$repository = {
+      users: {
+        getUsers: jest.fn().mockResolvedValue(mockResponse),
+      },
+    }
+
+    await actions.getUsers({ commit, state })
+    expect(commit).toHaveBeenCalledWith('setIsLoadingUsers', true)
+    expect(commit).toHaveBeenCalledWith('setUsers', mockResponse)
+    expect(commit).toHaveBeenCalledWith('setIsLoadingUsers', false)
+  })
+
+  test('actions: getUsers - Unhappy Path', async () => {
+    const state = {
+      users: [],
+      isLoadingUsers: false,
+    }
+    const commit = jest.fn()
+
+    actions.$repository = {
+      users: {
+        getUsers: jest.fn().mockRejectedValue(new Error('Error')),
+      },
+    }
+
+    await actions.getUsers({ commit, state })
+    expect(commit).toHaveBeenCalledWith('setIsLoadingUsers', true)
+    expect(commit).toHaveBeenCalledWith('setIsLoadingUsers', false)
   })
 })
